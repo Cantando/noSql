5de263787029ea1783e80ab6254284a9b7361dd2
Cantando <65988365+Cantando@users.noreply.github.com>
2020-07-31 21:37:22 -0400

    api getr method complete

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,3 +1,13 @@
 const db = require("../model");
 const router = require("express").Router();
 
+// get method for workouts to appear on page
+router.get("/workouts",(req,res)=>{
+    db.Workout.find({},(err,dbExercises)=>{
+        if (err){
+            console.log(err);
+        }
+        res.json(dbExercises);
+        console.log(dbExercises);
+    })
+})
