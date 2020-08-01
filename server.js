57d924ca46453b0eec112edd03dd1acb29125e67
Cantando <65988365+Cantando@users.noreply.github.com>
2020-07-31 22:21:40 -0400

    html routes working

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require("express");
+const express= require("express");
 const logger = require("morgan");
 const mongoose = require('mongoose');
 const routes = require("./routes");
@@ -6,7 +6,7 @@ const routes = require("./routes");
 const PORT = process.env.PORT || 3000;
 //Setting up app and model require
 
-const app = express();
+const app =express();
 //activate logger
 app.use(logger("dev"));
 //activate express
@@ -18,7 +18,8 @@ app.use(express.static("public"));
 mongoose.connect(process.env.MONGODB_URI || "", { useNewUrlParser: true, useUnifiedTopology: true });
 
 //use external routes folder 
-app.use(routes)
+
+app.use(routes);
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
