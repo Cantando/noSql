6c355557eed965a70ce542c2d4669cba1c679733
Cantando <65988365+Cantando@users.noreply.github.com>
2020-08-01 11:31:59 -0400

    finished on api routes and working on errs

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,13 +1,53 @@
-// const db = require("../model");
-// const router = require("express").Router();
+const db = require("../model");
+const router = require("express").Router();
 
-// // get method for workouts to appear on page
-// router.get("/workouts",(req,res)=>{
-//     db.Workout.find({},(err,dbExercises)=>{
-//         if (err){
-//             console.log(err);
-//         }
-//         res.json(dbExercises);
-//         console.log(dbExercises);
-//     })
-// })
+// get method for workouts to appear on page
+router.get("/workouts",(req,res)=>{
+    db.Workout.find({},(err,dbExercises)=>{
+        if (err){
+            console.log(err);
+        }
+        res.json(dbExercises);
+        console.log(dbExercises);
+    });
+})
+
+//Put method for adding new exercises
+
+router.put("/workouts/:id", (req,res)=> {
+    db.Workout.findByIdAndUpdate(req.params.id, {$push: {
+        exercises:req.body
+    }},(err, dbExercises)=> {
+        if(err){
+            console.log(err);
+        }
+        res.json(dbExercises)
+    });
+})
+
+
+//Post Method for creating new exercises 
+
+router.post("/workouts", ({body},res)=>{
+    db.Workout.create(body,(err,dbExercises)=>{
+        if(err){
+            console.log(err);
+        }
+        res.json(dbExercises);
+    });
+})
+
+//Get request for the stats
+
+router.get("workouts/range", (req,res)=>{
+    db.Workout.find({}).sort({day: -1}).then(dbExercises =>{
+        res.json(dbExercises);
+        console.log(dbExercises);
+    }).catch(err=>{
+        res.json(err);
+
+    });
+})
+
+
+module.exports= router;
